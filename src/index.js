643ed31a258ac58050ec0b70feb37dbd57bcbe96
pao050988 <pao050988@gmail.com>
2023-01-11 20:27:54 -0500

    created link
    create link to github

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
     <div className="WeatherApp">
       <h1> Weather App </h1>
       <Weather />
-      
+      <a href="https://github.com/pao050988/weather-react-app">Open-source code</a>
     </div>
   </StrictMode>
 );
